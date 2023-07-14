5ce802cd133f23600fe54259092ca645f82a99ee
ppb2 <32199273+ppb2@users.noreply.github.com>
2023-07-14 10:45:55 +0800

    Update glados_check.py

--- glados_check.py
+++ glados_check.py
@@ -30,7 +30,7 @@ headers = {
   'accept-language': 'zh-CN,zh;q=0.9',
   'authorization': '85957412030771491941354582567954-982-1512',
   'content-type': 'application/json;charset=UTF-8',
-  'cookie': '_gid=GA1.2.271850584.1681198796; koa:sess=eyJ1c2VySWQiOjMyMzc4NCwiX2V4cGlyZSI6MTcwNzExODk4NzMwOSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=aPxv8aB8CL6ymvH8dYNkr3lN9Qw; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc; __stripe_mid=1d83459e-fc71-417a-810a-d462f6ae6a9a9a0ad2; __stripe_sid=e07c9303-157f-4cc4-a683-7c09e49e58fbdd1c01; _ga_CZFVKMNT9J=GS1.1.1681206844.4.1.1681206969.0.0.0; _ga=GA1.1.1628570063.1681198780',
+  'cookie': 'koa:sess=eyJ1c2VySWQiOjMyMzc4NCwiX2V4cGlyZSI6MTcwNzExODk4NzMwOSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=aPxv8aB8CL6ymvH8dYNkr3lN9Qw; __stripe_mid=1d83459e-fc71-417a-810a-d462f6ae6a9a9a0ad2; _gid=GA1.2.792979677.1689217328; _gat_gtag_UA_104464600_2=1; _ga=GA1.1.1628570063.1681198780; _ga_CZFVKMNT9J=GS1.1.1689302592.15.1.1689302599.0.0.0',
   'dnt': '1',
   'origin': 'https://glados.rocks',
   'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
